dcb05768f56840c7092b59d435d3105f513eff8d
avinash751 <avi.g751@gmail.com>
2022-08-10 11:09:24 +0400

    input bindings  for controller made
    controller implementation not done

--- Assets/ControlerInput/ControllerInput.cs
+++ Assets/ControlerInput/ControllerInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hello : MonoBehaviour
+public class ControllerInput : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
