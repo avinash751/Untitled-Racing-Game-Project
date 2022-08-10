68f3751f4882fd59aa882de56bf49608649f8b36
Zapollo15 <a.roberts@student.saedubai.com>
2022-08-10 19:27:40 +0400

    Version 1.2

--- Assets/Spike Interaction/SpikeDeathController.cs
+++ Assets/Spike Interaction/SpikeDeathController.cs
@@ -15,4 +15,12 @@ public class SpikeDeathController : MonoBehaviour
     {
         
     }
+
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+
+        }
+    }
 }
