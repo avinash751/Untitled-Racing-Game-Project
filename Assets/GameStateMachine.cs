b368e3b511bb60e6fb4e193f280e84ebb40723d3
HatAYE <h.alchaban@student.saedubai.com>
2022-08-12 13:04:08 +0400

    state machine

---
+++ Assets/GameStateMachine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateMachine : MonoBehaviour
+{
+    public GameState gamestate;
+
+
+    public enum GameState
+    {
+        Start,
+        Current,
+        Gameover
+    }
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        gamestate = GameState.Start;
+    }
+
+    // Update is called once per frame
+    void CurrentGame()
+    {
+        gamestate = GameState.Current;
+    }
+
+    public void GameOver()
+    {
+        gamestate = GameState.Gameover;
+    }
+}
