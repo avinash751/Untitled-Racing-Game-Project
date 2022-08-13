2625294bc88f9beaf72d249195af6ca90977dc12
avinash751 <avi.g751@gmail.com>
2022-08-13 20:20:29 +0400

    chnages

--- Assets/GamePlayAssets/Players Folder/Controls.cs
+++ Assets/GamePlayAssets/Players Folder/Controls.cs
@@ -14,8 +14,7 @@ public class Controls : MonoBehaviour
     float DashingPower = 30f;
     float DashingTime = 0.3f;
     float DashingCooldown = 1f;
-    public InputActionAsset normal;
-    public InputActionAsset reversed;
+  
 
 
     void Start()
